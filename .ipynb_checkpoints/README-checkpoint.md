9fbbba49bc936e0886eab2bdcc9d66e3226ebdd4
aeishah <sitinoraeishah24b@forward.edu.my>
2024-12-29 18:49:41 +0800

    second commit

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -1,1 +1,84 @@
-# Brain MRI Diagnosis Prediction App
+# Hotel Price Prediction
+
+Welcome to the **Hotel Price Prediction** repository! This project leverages machine learning to predict hotel prices based on various features and input parameters. The application is built with Python and features a user-friendly interface using **Streamlit**.
+
+---
+
+## Table of Contents
+- [Overview](#overview)
+- [Features](#features)
+- [Technologies Used](#technologies-used)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+---
+
+## Overview
+The **Hotel Price Prediction** application aims to provide insights into hotel pricing trends and predict the cost of a hotel stay based on user-provided inputs. It uses machine learning models trained on historical data to deliver accurate predictions. This project is designed for users who want to explore and understand hotel pricing dynamics.
+
+---
+
+## Features
+- User-friendly interface powered by **Streamlit**.
+- Predict hotel prices based on input parameters such as location, star rating, accommodation type, and more.
+- Visualization of predictions and data insights.
+- Built with modular code for easy scalability and updates.
+
+---
+
+## Technologies Used
+- **Python**: Core programming language.
+- **Playwright & BeautifulSoup**: For scraping data from [Trivago](https://www.trivago.com.my/)
+- **Streamlit**: For creating the interactive user interface.
+- **Scikit-learn**: For building and training the machine learning model.
+- **Pandas**: For data manipulation and analysis.
+- **NumPy**: For numerical computations.
+- **Matplotlib/Seaborn**: For data visualization.
+
+---
+
+## Installation
+
+### Prerequisites
+- Python 3.7 or higher installed on your machine.
+- Git installed (optional, for cloning the repository).
+
+### Steps
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/aeishaharbee/cu5-project.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd cu5-project
+   ```
+4. Install the required dependencies:
+   ```bash
+   pip install streamlit pandas joblib plotly holidays datetime 
+   ```
+
+---
+
+## Usage
+
+1. Run the **Streamlit** application:
+   ```bash
+   streamlit run app.py
+   ```
+
+2. Open the provided local URL in your web browser (e.g., `http://localhost:8501`).
+
+3. Input the required parameters in the web app to predict hotel prices and visualize the results.
+
+---
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
+## Acknowledgments
+- Special thanks to the contributors of **Streamlit**, **Scikit-learn**, and other libraries used in this project.
+
--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -2,29 +2,42 @@ import streamlit as st
 import pandas as pd
 import joblib
 import plotly.express as px
+import holidays
+import datetime
+import base64
 
 pipeline = joblib.load('hotel_price_prediction_model.pkl')
 
 st.set_page_config(page_title="Hotel Price Prediction", layout="wide")
 st.sidebar.title("Hotel Price Prediction")
 page = st.sidebar.selectbox("", ["Home", "Data Visualization", "Predict"])
-st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
 
 df = pd.read_csv('cleaned_hotels_data.csv')
+holidays_dates = holidays.MY()
 
 # Home Page
 if page == "Home":
     st.title("Hotel Price Prediction App")
     st.write("""
         Welcome to the Hotel Price Prediction App. 
-        Use this tool to:
-        - Visualize data trends in hotel pricing.
-        - Predict hotel prices based on various features.
-        Navigate through the options in the sidebar to explore.
     """)
-
+    
+    background_image = """
+    <style>
+    [data-testid="stAppViewContainer"] > .main {
+        background-image: url("https://static.vecteezy.com/system/resources/previews/014/238/314/large_2x/dark-hotel-room-with-yellow-bulb-light-free-photo.jpg");
+        background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+        background-position: center;  
+        background-repeat: no-repeat;
+    }
+    </style>
+    """
+    
+    st.markdown(background_image, unsafe_allow_html=True)
+    
 # Data Visualization Page
 elif page == "Data Visualization":
+    st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
     st.title("Data Visualization")
     st.write("Here are some of the visualizations of the hotel price trends and patterns.")
     st.markdown("<br><br>", unsafe_allow_html=True)
@@ -42,6 +55,7 @@ elif page == "Data Visualization":
             xaxis_title="Accommodation Type", 
             yaxis_title="Average Price Per Night (RM)", 
         )
+        fig.update_traces(marker_color="#FF4B4B")
         st.plotly_chart(fig,use_container_width=True, height = 200)
     
     with col2:
@@ -55,6 +69,7 @@ elif page == "Data Visualization":
                 tickangle=45  # Rotate x-axis labels by 45 degrees
             ),
         )
+        fig.update_traces(marker_color="#FF4B4B")
         st.plotly_chart(fig,use_container_width=True, height = 200)
     st.markdown("<br><br><br><br>", unsafe_allow_html=True)
 
@@ -95,7 +110,7 @@ elif page == "Data Visualization":
         xaxis_title=x_title,
         yaxis_title="Average Price Per Night ($)",
     )
-    fig.update_traces(mode="lines+markers")
+    fig.update_traces(mode="lines+markers", marker_color="#FF4B4B", line_color="#FF4B4B")
     st.plotly_chart(fig, use_container_width=True)
     st.markdown("<br><br><br><br>", unsafe_allow_html=True)
 
@@ -107,6 +122,7 @@ elif page == "Data Visualization":
         fig1 = px.scatter(df, x='user_average_rating', y='price_per_night', 
                           labels={'user_average_rating': 'User Average Rating', 
                                   'price_per_night': 'Price Per Night'})
+        fig1.update_traces(marker_color="#FF4B4B")
         st.plotly_chart(fig1, use_container_width=True)
     
     with col2:
@@ -114,10 +130,12 @@ elif page == "Data Visualization":
         fig2 = px.scatter(df, x='hotel_rating', y='price_per_night', 
                           labels={'hotel_rating': 'Hotel Rating', 
                                   'price_per_night': 'Price Per Night'})
+        fig2.update_traces(marker_color="#FF4B4B")
         st.plotly_chart(fig2, use_container_width=True)
 
 # Predict Page
 elif page == "Predict":
+    st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
     if "input_data" not in st.session_state:
         st.session_state.input_data = {
             'accommodation_type': 'Hotel',
@@ -127,26 +145,17 @@ elif page == "Predict":
             'city': 'Georgetown',
             'rooms': 1,
             'guests': 2,
-            'free_wifi': 1,
-            'pool': 1,
-            'parking': 0,
-            'aircond': 1,
-            'restaurant': 1,
-            'hotel_bar': 1,
-            'balcony_patio': 0,
-            'spa': 0,
-            'gym': 0,
-            'hairdryer': 1,
-            'kitchen': 0,
-            'non_smoking_room': 1,
-            'tv_ent': 1,
-            'washing_machine': 0,
-            'holiday': 0,
-            'month': 12,
-            'day': 28,
-            'day_of_week': 5
+            'date': datetime.date.today(),
+            'balcony_patio': 1, 'gym': 1, 'aircond': 1, 'pool': 1,
+            'restaurant': 1, 'non_smoking_room': 1, 'spa': 1, 'kitchen': 1,
+            'hotel_bar': 1, 'hairdryer': 1, 'parking': 1, 'tv_ent': 1,
+            'free_wifi': 1, 'washing_machine': 1,
         }
     
+    st.title("Predict Hotel Price")
+    st.write("Predict hotel prices using our trained model. Choose an example or enter custom inputs.")
+    
+    # Example data
     example_1 = {
         'accommodation_type': 'Hotel',
         'hotel_rating': 4,
@@ -155,24 +164,11 @@ elif page == "Predict":
         'city': 'Georgetown',
         'rooms': 1,
         'guests': 2,
-        'free_wifi': 1,
-        'pool': 1,
-        'parking': 0,
-        'aircond': 1,
-        'restaurant': 1,
-        'hotel_bar': 1,
-        'balcony_patio': 0,
-        'spa': 0,
-        'gym': 0,
-        'hairdryer': 1,
-        'kitchen': 0,
-        'non_smoking_room': 1,
-        'tv_ent': 1,
-        'washing_machine': 0,
-        'holiday': 0,
-        'month': 12,
-        'day': 28,
-        'day_of_week': 5
+        'date': datetime.date(2023, 12, 28),
+        'balcony_patio': 1, 'gym': 1, 'aircond': 1, 'pool': 1,
+        'restaurant': 1, 'non_smoking_room': 1, 'spa': 1, 'kitchen': 1,
+        'hotel_bar': 1, 'hairdryer': 1, 'parking': 1, 'tv_ent': 1,
+        'free_wifi': 1, 'washing_machine': 1,
     }
     
     example_2 = {
@@ -183,28 +179,12 @@ elif page == "Predict":
         'city': 'Kuala Lumpur',
         'rooms': 2,
         'guests': 4,
-        'free_wifi': 1,
-        'pool': 0,
-        'parking': 1,
-        'aircond': 1,
-        'restaurant': 0,
-        'hotel_bar': 0,
-        'balcony_patio': 1,
-        'spa': 0,
-        'gym': 0,
-        'hairdryer': 1,
-        'kitchen': 1,
-        'non_smoking_room': 1,
-        'tv_ent': 1,
-        'washing_machine': 1,
-        'holiday': 1,
-        'month': 5,
-        'day': 12,
-        'day_of_week': 3
+        'date': datetime.date(2024, 5, 12),
+        'balcony_patio': 1, 'gym': 1, 'aircond': 1, 'pool': 1,
+        'restaurant': 1, 'non_smoking_room': 1, 'spa': 1, 'kitchen': 1,
+        'hotel_bar': 1, 'hairdryer': 1, 'parking': 1, 'tv_ent': 1,
+        'free_wifi': 1, 'washing_machine': 1,
     }
-    
-    st.title("Predict Hotel Price")
-    st.write("Predict hotel prices using our trained model. Choose an example or enter custom inputs.")
     
     if st.button("Use Example 1"):
         st.session_state.input_data = example_1
@@ -225,9 +205,15 @@ elif page == "Predict":
         st.session_state.input_data['hotel_rating'] = st.selectbox(
             "Hotel Rating (1-5)", [1, 2, 3, 4, 5], index=st.session_state.input_data['hotel_rating'] - 1
         )
-        st.session_state.input_data['user_average_rating'] = st.slider(
-            "User Average Rating (0-10)", 0.0, 10.0, st.session_state.input_data['user_average_rating']
-        )
+        selected_date = st.date_input("Pick a Date", value=st.session_state.input_data['date'])
+        st.session_state.input_data['date'] = selected_date
+        st.session_state.input_data['holiday'] = 1 if selected_date in holidays_dates else 0
+        st.session_state.input_data['day_of_week'] = selected_date.weekday()
+        st.session_state.input_data['month'] = selected_date.month
+        st.session_state.input_data['day'] = selected_date.day
+    
+    with col2:
+        st.session_state.input_data['rooms'] = st.number_input("Rooms", min_value=1, value=st.session_state.input_data['rooms'])
         st.session_state.input_data['user_comment'] = st.selectbox(
             "User Comment", ['Excellent', 'Very good', 'Good', 'Okay', 'Fair'], index=['Excellent', 'Very good', 'Good', 'Okay', 'Fair'].index(st.session_state.input_data['user_comment'])
         )
@@ -235,34 +221,18 @@ elif page == "Predict":
             "City", ['Kuala Lumpur', 'Malacca', 'Kota Kinabalu', 'Genting Highlands', 'Johor Bahru', 'Ipoh', 'Kuantan', 'Kuching', 'Georgetown', 'Putrajaya'],
             index=['Kuala Lumpur', 'Malacca', 'Kota Kinabalu', 'Genting Highlands', 'Johor Bahru', 'Ipoh', 'Kuantan', 'Kuching', 'Georgetown', 'Putrajaya'].index(st.session_state.input_data['city'])
         )
-        st.session_state.input_data['rooms'] = st.number_input("Rooms", min_value=1, value=st.session_state.input_data['rooms'])
-        st.session_state.input_data['guests'] = st.number_input("Guests", min_value=1, value=st.session_state.input_data['guests'])
-    
-    with col2:
-        for key in ['free_wifi', 'pool', 'parking', 'aircond', 'restaurant', 'hotel_bar', 'balcony_patio', 'spa', 'gym', 'hairdryer', 'kitchen']:
-            current_value = st.session_state.input_data[key]
-            st.session_state.input_data[key] = st.selectbox(
-                f"{key.replace('_', ' ').title()} (1=Yes, 0=No)",
-                [1, 0],
-                index=[1, 0].index(current_value)  # Explicitly calculate index
-            )
+        
     
     with col3:
-        for key in ['non_smoking_room', 'tv_ent', 'washing_machine', 'holiday']:
-            current_value = st.session_state.input_data[key]
-            st.session_state.input_data[key] = st.selectbox(
-                f"{key.replace('_', ' ').title()} (1=Yes, 0=No)",
-                [1, 0],
-                index=[1, 0].index(current_value)  # Explicitly calculate index
-            )
-
-            
-        st.session_state.input_data['month'] = st.number_input("Month", min_value=1, max_value=12, value=st.session_state.input_data['month'])
-        st.session_state.input_data['day'] = st.number_input("Day", min_value=1, max_value=31, value=st.session_state.input_data['day'])
-        st.session_state.input_data['day_of_week'] = st.number_input("Day of Week (0=Monday)", min_value=0, max_value=6, value=st.session_state.input_data['day_of_week'])
+        st.session_state.input_data['guests'] = st.number_input("Guests", min_value=1, value=st.session_state.input_data['guests'])
+        st.session_state.input_data['user_average_rating'] = st.slider(
+            "User Average Rating (0-10)", 0, 10, int(st.session_state.input_data['user_average_rating']), step=1
+        )
+        
     
     input_data_df = pd.DataFrame([st.session_state.input_data])
     
     if st.button("Predict"):
+        input_data_df = input_data_df.drop(columns=['date'])
         prediction = pipeline.predict(input_data_df)
         st.write(f"Predicted Price per Night: RM {prediction[0]:.2f}")
